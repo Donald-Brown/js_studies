3f1a6c4a4fc90b351b8815a539f02df18280003d
donald-brown <donbrown4652@gmail.com>
2020-11-12 06:16:23 -0600

    adds priority queue starts binary search tree

--- my_data_structures/priority_queue/priority_queue.js
+++ my_data_structures/priority_queue/priority_queue.js
@@ -7,8 +7,22 @@ function PriorityQueue() {
     console.log(collection);
   }
 
-  this.enqueue = function() {
-
+  this.enqueue = function(element) {
+    if(collection.length === 0){
+      collection.push(element);
+    }else{
+      let added = false;
+      for(let i = 0; i < collection.length; i++){
+        if(element[1] < collection[i][1]){
+          collection.splice(i, 0, element);
+          added = true;
+          break;
+        }
+      }
+      if(!added){
+        collection.push(element);
+      }
+    }
   }
 
   this.dequeue = function() {
@@ -28,3 +42,17 @@ function PriorityQueue() {
     return (collection.length === 0);
   }
 }
+
+let myPriorityQ = new PriorityQueue();
+console.log(myPriorityQ.isEmpty());
+console.log(myPriorityQ.size());
+myPriorityQ.enqueue(['size', 3]);
+myPriorityQ.enqueue(['shape', 1]);
+myPriorityQ.print();
+myPriorityQ.enqueue(['color', 1]);
+myPriorityQ.enqueue(['width', 2])
+myPriorityQ.print();
+// console.log(myPriorityQ.dequeue());
+// myPriorityQ.print();
+console.log(myPriorityQ.front());
+console.log(myPriorityQ.isEmpty());
