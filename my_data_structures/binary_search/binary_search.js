7e17dd959e5f60a52c5f43d5010bdbd0021cd82d
donald-brown <donbrown4652@gmail.com>
2020-11-16 06:15:19 -0600

    continues work on binary search tree

--- my_data_structures/binary_search/binary_search.js
+++ my_data_structures/binary_search/binary_search.js
@@ -115,39 +115,120 @@ class BinarySearchTree {
     }
     this.root = removeNode(this.root, data);
   }
+  findMinHeight(node = this.root) {
+    if (node === null) {
+      return -1;
+    }
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
+    if (left < right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+  findMaxHeight(node = this.root) {
+    if (node === null) {
+      return -1;
+    }
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+    if (left > right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+  isBalanced() {
+    return this.findMinHeight() >= this.findMaxHeight() - 1;
+  }
+  inOrder() {
+    if (this.root === null) {
+      return null;
+    } else {
+      let result = new Array();
+      function traverseInOrder(node) {
+        node.left && traverseInOrder(node.left);
+        result.push(node.data);
+        node.right && traverseInOrder(node.right);
+      }
+      traverseInOrder(this.root);
+      return result;
+    }
+  }
+  preOrder() {
+
+  }
+  postOrder() {
+
+  }
+  levelOrder() {
+
+  }
 }
 
-let myTree = new BinarySearchTree();
-console.log(myTree);
-myTree.add(15);
-console.log(myTree);
+// let myTree = new BinarySearchTree();
+// console.log(myTree);
+// myTree.add(15);
+// console.log(myTree);
 
-myTree.add(34);
-console.log(myTree);
-myTree.add(4);
-myTree.add(10);
-myTree.add(25);
-myTree.add(2);
-myTree.add(7);
-console.log(myTree);
+// myTree.add(34);
+// console.log(myTree);
+// myTree.add(4);
+// myTree.add(10);
+// myTree.add(25);
+// myTree.add(2);
+// myTree.add(7);
+// console.log(myTree);
 
-console.log(myTree.findMin());
-console.log(myTree.findMax());
+// console.log(myTree.findMin());
+// console.log(myTree.findMax());
 
-console.log(myTree.find(3));
-console.log(myTree.find(4));
-console.log(myTree.find(25));
+// console.log(myTree.find(3));
+// console.log(myTree.find(4));
+// console.log(myTree.find(25));
 
-console.log(myTree.isPresent(3));
-console.log(myTree.isPresent(25));
+// console.log(myTree.isPresent(3));
+// console.log(myTree.isPresent(25));
 
-console.log('space');
-console.log(myTree.isPresent(2));
-myTree.remove(2);
-console.log(myTree.isPresent(2));
+// console.log('space');
+// console.log(myTree.isPresent(2));
+// myTree.remove(2);
+// console.log(myTree.isPresent(2));
 
-console.log('space')
+// console.log('space')
 
-console.log(myTree.isPresent(25));
-myTree.remove(25);
-console.log(myTree.isPresent(25));
+// console.log(myTree.isPresent(25));
+// myTree.remove(25);
+// console.log(myTree.isPresent(25));
+
+let bst = new BinarySearchTree();
+bst.add(9);
+bst.add(4);
+bst.add(17);
+bst.add(3);
+bst.add(6);
+bst.add(22);
+bst.add(5);
+bst.add(7);
+bst.add(20);
+
+// console.log(bst.findMinHeight());
+// console.log(bst.findMaxHeight());
+// console.log(bst.isBalanced());
+// bst.add(10);
+// console.log(bst.findMinHeight());
+// console.log(bst.findMaxHeight());
+// console.log(bst.isBalanced());
+console.log(bst.inOrder());
+
+
+
+// console.log('space');
+// let bst2 = new BinarySearchTree();
+// bst2.add(5);
+// bst2.add(3);
+// bst2.add(7);
+// console.log(bst2.findMinHeight());
+// console.log(bst2.findMaxHeight());
+// console.log(bst2.isBalanced());
