fd1b1d128737565f4f907a0ceb8d9c87d236a2a7
donald-brown <donbrown4652@gmail.com>
2020-12-23 06:16:39 -0600

    works on canContstuct and countConstruct

--- problems/memoization/canContstuct/canConstruct1/canConstruct1.js
+++ problems/memoization/canContstuct/canConstruct1/canConstruct1.js
@@ -18,3 +18,28 @@ console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])); // true
 console.log(canConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])); // false
 console.log(canConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'])); //true
 //console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'])); //false
+
+console.log('memoized solutions');
+//* memoized solution
+const canConstructBetter = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (target === '') return true;
+
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      if (canConstructBetter(suffix, wordBank, memo)) { //? truthy value
+        memo[target] = true;
+        return true;
+      }
+    }
+  }
+
+  memo[target] = false;
+  return false;
+}
+
+console.log(canConstructBetter('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])); // true
+console.log(canConstructBetter('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])); // false
+console.log(canConstructBetter('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'])); //true
+console.log(canConstructBetter('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'])); //false
