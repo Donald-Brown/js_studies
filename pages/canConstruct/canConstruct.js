7d20316154b79dced48ce35f20ab0834d0019dbc
donald-brown <donbrown4652@gmail.com>
2020-12-24 06:15:54 -0600

    works on countConstruct

--- pages/canConstruct/canConstruct.js
+++ pages/canConstruct/canConstruct.js
@@ -1,16 +1,15 @@
 //* Brute force solution
-const canConstruct = (target, wordBank) => {
+const canConstruct = (target, words) => {
   if (target === '') return true;
 
-  for (let word of wordBank) {
-    if (target.indexOf(word) === 0){
-      const suffix = target.slice(word.length);
-      if (canConstruct(suffix, wordBank) === true) {
+  for (let word of words) {
+    if (target.indexOf(word) === 0) {
+      suffix = target.slice(word.length);
+      if (canConstruct(suffix, words) === true) {
         return true;
       }
     }
   }
-
   return false;
 }
 
@@ -20,3 +19,24 @@ console.log(canConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o'
 //console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'])); //false
 
 //* memoized solution
+const canConstructBetter = (target, words, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (target === '') return true;
+
+  for (let word of words) {
+    if (target.indexOf(word) === 0) {
+      suffix = target.slice(word.length);
+      if (canConstructBetter(suffix, words, memo) === true) {
+        memo[target] = true;
+        return true;
+      }
+    }
+  }
+  memo[target] = false;
+  return false;
+}
+
+console.log(canConstructBetter('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])); // true
+console.log(canConstructBetter('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])); // false
+console.log(canConstructBetter('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'])); //true
+console.log(canConstructBetter('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'])); //false
