11bf91d826945cc62ea85c949f06c0c171cc0938
donald-brown <donbrown4652@gmail.com>
2020-12-29 05:40:22 -0600

    works on counConstruct Problem

---
+++ pages/countConstruct/countConstruct.js
@@ -0,0 +1,44 @@
+//* brute force solution 
+const countConstruct = (target, wordBank) => {
+  if (target === '') return 1;
+  let totalCount = 0;
+
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const numberOfWaysForRest = countConstruct(target.slice(word.length), wordBank);
+      totalCount += numberOfWaysForRest;
+    }
+  }
+  return totalCount;
+}
+
+console.log(countConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl'])); // 2
+console.log(countConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])); // 1
+console.log(countConstruct('skateBoard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])); // 0
+console.log(countConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'])); // 4
+//console.log(countConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'])); // 0
+
+console.log('New Function');
+
+//* memoized solution
+const countConstructBetter = (target, wordBank, memo = {}) => {
+  if (target in memo) return memo[target];
+  if (target === '') return 1;
+
+  let totalCount = 0;
+
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const numberOfWaysForRest = countConstructBetter(target.slice(word.length), wordBank, memo);
+      totalCount += numberOfWaysForRest;
+    }
+  }
+  memo[target] = totalCount;
+  return totalCount;
+}
+
+console.log(countConstructBetter('purple', ['purp', 'p', 'ur', 'le', 'purpl'])); // 2
+console.log(countConstructBetter('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd'])); // 1
+console.log(countConstructBetter('skateBoard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])); // 0
+console.log(countConstructBetter('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'])); // 4
+console.log(countConstructBetter('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'])); // 0
